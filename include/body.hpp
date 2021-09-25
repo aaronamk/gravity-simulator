9dbe6940196fb4c24f2f9daffe2fd3d9afa930db
aaronamk <amklein@email.wm.edu>
2021-09-24 22:56:37 -0400

    Revamp body code structure.

--- include/body.hpp
+++ include/body.hpp
@@ -6,12 +6,19 @@
 
 class Body {
 public:
-	Body(const gv::vect &pos, const float &radius, const float &mass);
+	Body(const gv::vect &pos, const float &radius, const float &mass)
+			: m_pos(pos), m_radius(radius), m_mass(mass) {};
+
+	void apply_force(gv::vect &F) { this->m_net_force += F; };
+
+	const gv::vect &net_force(void) { return this->m_net_force; };
+
 
 private:
-	gv::vect pos;
-	float radius;
-	float mass;
+	gv::vect m_pos;
+	float m_radius;
+	float m_mass;
+	gv::vect m_net_force = { 0, 0 };
 };
 
 
--- src/body.cpp
+++
@@ -1,8 +0,0 @@
-#include "body.hpp"
-#include "gv.hpp"
-
-Body::Body(const gv::vect &pos, const float &radius, const float &mass) {
-	this->pos = pos;
-	this->radius = radius;
-	this->mass = mass;
-}
