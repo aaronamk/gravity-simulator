da35c0cdc7787d434730f0ced1c299a471ebf92e
aaronamk <amklein@email.wm.edu>
2021-09-25 20:32:34 -0400

    Add drawable to body.

--- include/body.hpp
+++ include/body.hpp
@@ -1,13 +1,18 @@
 #ifndef BODY_HPP
 #define BODY_HPP
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
 #include "gv.hpp"
 
 
+/**
+ * An astranomical body
+ */
 class Body {
 public:
-	Body(const gv::vect &pos, const float &radius, const float &mass)
-			: _pos(pos), _radius(radius), _mass(mass) {};
+	Body(const gv::vect &pos, const float &radius, const float &mass);
 
 	const gv::vect &pos() { return _pos; };
 
@@ -15,9 +20,14 @@ public:
 
 	const float &mass() { return _mass; };
 
+	/**
+	 * Add a force to be applied in the next udpate cycle
+	 */
 	void apply_force(const gv::vect &force) { _net_force += force; };
 
 	void update();
+
+	void draw(sf::RenderWindow &window);
 
 
 private:
@@ -26,6 +36,8 @@ private:
 	float _mass;
 	gv::vect _velocity { 0, 0 };
 	gv::vect _net_force { 0, 0 };
+
+	sf::CircleShape _drawable;
 };
 
 
--- src/body.cpp
+++ src/body.cpp
@@ -2,6 +2,14 @@
 #include "gv.hpp"
 
 
+Body::Body(const gv::vect &pos, const float &radius, const float &mass)
+		: _pos(pos), _radius(radius), _mass(mass) {
+	_drawable.setOrigin(_pos.x, _pos.y);
+	_drawable.setRadius(_radius);
+	_drawable.setFillColor(sf::Color::Green);
+}
+
+
 void Body::update() {
 	// accelerate based on net force being applied
 	_velocity += gv::dt * gv::time_scale * _net_force / _mass;
@@ -9,4 +17,10 @@ void Body::update() {
 
 	// reset net force for the next update
 	_net_force = { 0, 0 };
+}
+
+
+void Body::draw(sf::RenderWindow &window) {
+	_drawable.setPosition(_pos.x, _pos.y);
+	window.draw(_drawable);
 }
